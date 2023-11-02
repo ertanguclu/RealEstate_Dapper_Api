fd1408121723b981ae903c3371e0f5f09a28dcc5
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2023-11-02 18:49:15 +0300

    Müşteri Yorumları Eklenmesi

--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -4,6 +4,7 @@ using RealEstate_Dapper_Api.Repositories.CategoryRepository;
 using RealEstate_Dapper_Api.Repositories.PopularLocationRepositories;
 using RealEstate_Dapper_Api.Repositories.ProductRepository;
 using RealEstate_Dapper_Api.Repositories.ServiceRepository;
+using RealEstate_Dapper_Api.Repositories.TestimonialRepositories;
 using RealEstate_Dapper_Api.Repositories.WhoWeAreRepository;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +18,7 @@ builder.Services.AddTransient<IWhoWeAreDetailRepository,WhoWeAreDetailRepository
 builder.Services.AddTransient<IServiceRepository,ServiceRepository>();
 builder.Services.AddTransient<IBottomGridRepository,BottomGridRepository>();
 builder.Services.AddTransient<IPopularLocationRepository,PopularLocationRepository>();
+builder.Services.AddTransient<ITestimonialRepository,TestimonialRepository>();
 
 
 builder.Services.AddControllers();
