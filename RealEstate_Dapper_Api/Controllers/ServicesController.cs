944bf6dbab7ff79092355ef4a78b8a60ce2c088e
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2023-11-02 12:02:22 +0300

    Service Api

---
+++ RealEstate_Dapper_Api/Controllers/ServicesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.ServiceRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly IServiceRepository _serviceRespository;
+        public ServicesController(IServiceRepository serviceRespository)
+        {
+            _serviceRespository = serviceRespository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetServiceList() 
+        {
+            var values = await _serviceRespository.GetAllServiceAsync();
+            return Ok(values);
+        }
+    }
+}
--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -1,6 +1,7 @@
 using RealEstate_Dapper_Api.Models.DapperContext;
 using RealEstate_Dapper_Api.Repositories.CategoryRepository;
 using RealEstate_Dapper_Api.Repositories.ProductRepository;
+using RealEstate_Dapper_Api.Repositories.ServiceRepository;
 using RealEstate_Dapper_Api.Repositories.WhoWeAreRepository;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,7 @@ builder.Services.AddTransient < Context>();
 builder.Services.AddTransient < ICategoryRepository,CategoryRepository>();
 builder.Services.AddTransient<IProductRepository,ProductRepository>();
 builder.Services.AddTransient<IWhoWeAreDetailRepository,WhoWeAreDetailRepository>();
+builder.Services.AddTransient<IServiceRepository,ServiceRepository>();
 
 
 builder.Services.AddControllers();
