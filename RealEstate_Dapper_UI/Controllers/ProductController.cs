081b7edce5edcb9d7b69fcc9200978de4cd7b1fd
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2023-11-05 19:22:03 +0300

    Ürün Listesi Api Consume İşlemi

--- RealEstate_Dapper_UI/Controllers/ProductController.cs
+++ RealEstate_Dapper_UI/Controllers/ProductController.cs
@@ -2,27 +2,32 @@
 using Newtonsoft.Json;
 using RealEstate_Dapper_UI.Dtos.ProductDtos;
 
-namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+namespace RealEstate_Dapper_UI.Controllers
 {
-    public class _DefaultHomePageProductList : ViewComponent
+    public class ProductController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public _DefaultHomePageProductList(IHttpClientFactory httpClientFactory)
+        public ProductController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task <IViewComponentResult> InvokeAsync()
+        public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:44352/api/Products/ProductListWithCategory");
             if(responseMessage.IsSuccessStatusCode)
             {
-                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
                 return View(values);
             }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct()
+        {
             return View();
         }
     }
