635fc8bcb3cadc3ebbd0e93f0001d1181f63e245
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2023-11-05 15:05:17 +0300

    Api Consume ile Yeni Kategori Eklenmesi

--- RealEstate_Dapper_UI/Controllers/CategoryController.cs
+++ RealEstate_Dapper_UI/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RealEstate_Dapper_UI.Dtos.CategoryDtos;
+using System.Text;
 
 namespace RealEstate_Dapper_UI.Controllers
 {
@@ -22,6 +23,25 @@ namespace RealEstate_Dapper_UI.Controllers
                 var jsonData= await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCAtegoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData= JsonConvert.SerializeObject(createCAtegoryDto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var resonseMessage = await client.PostAsync("https://localhost:44352/api/Categories", stringContent);
+            if(resonseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
