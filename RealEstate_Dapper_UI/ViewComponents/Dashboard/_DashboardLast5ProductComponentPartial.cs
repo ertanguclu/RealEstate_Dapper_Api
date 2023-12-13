ffff65e513fd071a26e6610dacc90601652729e3
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2023-12-13 14:27:49 +0300

    Dashboard İşlemleri

---
+++ RealEstate_Dapper_UI/ViewComponents/Dashboard/_DashboardLast5ProductComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.Dashboard
+{
+    public class _DashboardLast5ProductComponentPartial:ViewComponent
+    {
+        public async  Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
