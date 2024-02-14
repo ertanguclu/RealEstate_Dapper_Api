b5cebedec80003346751b886caa459829187f797
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-02-14 18:37:37 +0300

    Estate Agent Paneli

---
+++ RealEstate_Dapper_UI/Areas/EstateAgent/Controllers/MyAdvertsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.Areas.EstateAgent.Controllers
+{
+    public class MyAdvertsController : Controller
+    {
+        [Area("EstateAgent")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
