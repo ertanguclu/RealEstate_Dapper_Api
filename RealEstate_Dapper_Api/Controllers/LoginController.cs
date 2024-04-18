df0089b752312d5f295f1ee9036a4a87ec86b47f
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-04-18 12:33:25 +0300

    Giriş Yapan Kullanıcı İçin Apide JWT Oluşturma

---
+++ RealEstate_Dapper_Api/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Dtos.LoginDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+using RealEstate_Dapper_Api.Tools;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public LoginController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(CreateLoginDto loginDto)
+        {
+            string query = "SELECT * FROM AppUser WHERE Username = @username AND Password = @password";
+            string query2 = "SELECT UserId FROM AppUser WHERE Username = @username AND Password = @password";
+            var parameters = new DynamicParameters();
+            parameters.Add("@username", loginDto.Username);
+            parameters.Add("@password", loginDto.Password);
+            using(var connection= _context.CreateConnection())
+            {
+                var values = await connection.QueryFirstOrDefaultAsync<CreateLoginDto>(query, parameters);
+                var values2 = await connection.QueryFirstAsync<GetAppUserIdDto>(query2, parameters);
+                if(values != null)
+                {
+                    GetCheckAppUserViewModel model = new GetCheckAppUserViewModel();
+                    model.Username = values.Username;
+                    model.Id = values2.UserId;
+                    var token = JwtTokenGenerator.GenerateToken(model);
+                    return Ok(token);
+                }
+                return Ok("Başarısız");
+            }
+
+        }
+    }
+}
