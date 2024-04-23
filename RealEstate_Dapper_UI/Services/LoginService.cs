45fa79a7f407cf512538731b22f23e02d40e06e3
Ertan GÜÇLÜ <ertan.guclu@std.yildiz.edu.tr>
2024-04-23 15:46:37 +0300

    Giriş Yapan Kullanıcının Jwt ile ID'sini Getirme

---
+++ RealEstate_Dapper_UI/Services/LoginService.cs
@@ -0,0 +1,14 @@
+﻿namespace RealEstate_Dapper_UI.Services
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public LoginService(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        public string GetUserId => _contextAccessor.HttpContext.User.FindFirst("sub").Value;
+    }
+}
