36297aa7ac03a06b2c7e3e88de9a05be967f56ad
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-08-25 18:20:50 +0300

    Property Single Sayfasında Emlakçı Bilgileri Eklendi

---
+++ RealEstate_Dapper_Api/Controllers/AppUsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.AppUserRepositories;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppUsersController : ControllerBase
+    {
+        private readonly IAppUserRepository _appUserRepository;
+        public AppUsersController(IAppUserRepository appUserRepository)
+        {
+            _appUserRepository = appUserRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAppUserByProductId(int id)
+        {
+            var values = await _appUserRepository.GetAppUserByProductId(id);
+            return Ok(values);
+        }
+    }
+}
--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -1,5 +1,6 @@
 using RealEstate_Dapper_Api.Hubs;
 using RealEstate_Dapper_Api.Models.DapperContext;
+using RealEstate_Dapper_Api.Repositories.AppUserRepositories;
 using RealEstate_Dapper_Api.Repositories.BottomGridRepositories;
 using RealEstate_Dapper_Api.Repositories.CategoryRepository;
 using RealEstate_Dapper_Api.Repositories.ContactRepositories;
@@ -39,6 +40,7 @@ builder.Services.AddTransient<IChartRepository, ChartRepository>();
 builder.Services.AddTransient<ILast5ProductsRepository, Last5ProductsRepository>();
 builder.Services.AddTransient<IMessageRepository, MessageRepository>();
 builder.Services.AddTransient<IProductImageRepository, ProductImageRepository>();
+builder.Services.AddTransient<IAppUserRepository, AppUserRepository>();
 
 builder.Services.AddCors(opt =>
 {
