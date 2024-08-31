d769d392debe0092b5b3a06d912c081aafeac717
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-08-31 16:06:04 +0300

    Property Amenities Api Kısmı

---
+++ RealEstate_Dapper_Api/Controllers/PropertyAmenities.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.PropertyAmenityRepositories;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertyAmenities : ControllerBase
+    {
+        private readonly IPropertyAmenityRepository _propertyAmenityRepository;
+
+        public PropertyAmenities(IPropertyAmenityRepository propertyAmenityRepository)
+        {
+            _propertyAmenityRepository = propertyAmenityRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ResultPropertyAmenityByStatusTrue(int id)
+        {
+            var values = await _propertyAmenityRepository.ResultPropertyAmenityByStatusTrue(id);
+            return Ok(values);
+        }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/PropertyAmenityRepositories/PropertyAmenityRepository.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.PropertyAmenityDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.PropertyAmenityRepositories
+{
+    public class PropertyAmenityRepository : IPropertyAmenityRepository
+    {
+        private readonly Context _context;
+        public PropertyAmenityRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultPropertyAmenityByStatusTrueDto>> ResultPropertyAmenityByStatusTrue(int id)
+        {
+            string query = "SELECT PropertyAmenityId,Title FROM PropertyAmenity Inner Join Amenity on Amenity.AmenityId = PropertyAmenity.AmenityId" +
+                " WHERE PropertyId= @propertyId And Status= 1";
+            var parameters = new DynamicParameters();
+            parameters.Add("@propertyId", id);
+            using (var connection = _context.CreateConnection())
+            {
+                var result = await connection.QueryAsync<ResultPropertyAmenityByStatusTrueDto>(query, parameters);
+                return result.ToList();
+            }
+        }
+    }
+}
