bfbef7cfc83f6691a18ae7398840dbe6faa5c858
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-09-10 17:43:17 +0300

    Ana sayfa subfeature alanı dinamik hale getirildi

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultSubFeatureComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultSubFeatureComponentPartial.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.SubFeatureDtos;
 
 namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
     public class _DefaultSubFeatureComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultSubFeatureComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44352/api/SubFeatures");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSubFeatureDto>>(jsonData);
+                return View(values);
+            }
+            return View(new List<ResultSubFeatureDto>());
         }
     }
 }
