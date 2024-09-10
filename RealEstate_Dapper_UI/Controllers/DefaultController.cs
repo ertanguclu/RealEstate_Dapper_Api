5baf4f2fc359456794e2d64a07548bebe0f442d0
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-09-10 12:08:36 +0300

    UI'da Filtreleme Hazırlığı

--- RealEstate_Dapper_UI/Controllers/DefaultController.cs
+++ RealEstate_Dapper_UI/Controllers/DefaultController.cs
@@ -8,5 +8,17 @@ namespace RealEstate_Dapper_UI.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public PartialViewResult PartialSearch()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public IActionResult PartialSearch(string p)
+        {
+            TempData["word"] = p;
+            return RedirectToAction("PropertyListWithSearch", "Property");
+        }
     }
 }
