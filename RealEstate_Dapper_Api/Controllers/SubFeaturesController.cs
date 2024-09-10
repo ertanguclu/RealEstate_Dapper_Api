aa23cce898afd962ea13bfccaf5fe71625a97945
Ertan GÜÇLÜ <[ ertan.guclu@std.yildiz.edu.tr]>
2024-09-10 16:07:12 +0300

    Ana Sayfada Sub Feature alanı düzenlemeleri

---
+++ RealEstate_Dapper_Api/Controllers/SubFeaturesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.SubFeatureRepositories;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubFeaturesController : ControllerBase
+    {
+        private readonly ISubFeatureRepository _subFeatureRepository;
+        public SubFeaturesController(ISubFeatureRepository subFeatureRepository)
+        {
+            _subFeatureRepository = subFeatureRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllSubFeatureList()
+        {
+            var result = await _subFeatureRepository.GetAllSubFeatureAsync();
+            return Ok(result);
+        }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/SubFeatureRepositories/SubFeatureRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.SubFeatureDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.SubFeatureRepositories
+{
+    public class SubFeatureRepository : ISubFeatureRepository
+    {
+        private readonly Context _context;
+        public SubFeatureRepository(Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<ResultSubFeatureDto>> GetAllSubFeatureAsync()
+        {
+            string query = "SELECT * FROM SubFeature";
+            using (var connection = _context.CreateConnection())
+            {
+                var result = await connection.QueryAsync<ResultSubFeatureDto>(query);
+                return result.ToList();
+            }
+        }
+    }
+}
